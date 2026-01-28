f88ff36a23d729ca92ddf9ba880701ebca62d843
Anrietta <sekereshai@gmai.com>
2026-01-28 16:51:26 +0200

    Add solutions 18 Tasks 1-3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,72 @@
-// 17. JS. DOM. Створення DOM-елементів
 
-// 1. За допомогою DOM створити елемент section. Встановити у нього атрибут id зі 
-// значенням “root”. Всередину цього елемента прописати довільний рядок тексту. 
-// Встановити колір тексту для елемента. Відобразити елемент, зробивши його 
-// дочірнім до <body>
+// -------------------------------------------------------------------------------------------------------------------------------
 
 
-const sectionEl = document.createElement('section');
-sectionEl.id = 'root';
-sectionEl.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing.';
-sectionEl.style.color = 'red';
-document.body.append(sectionEl);
+// 18. JS. DOM. Події
+
+// 1. Прописати у html-розмітці кнопку (<button>) з будь-яким селектором (класс, id). 
+// За допомогою DOM отримати посилання на цей елемент та навісити на нього обробник події кліку.
+//  За кліком кнопка має викликати модальне вікно (alert) з текстом “Привіт тобі, клацальщик!”
+
+const button = document.querySelector('#btn');
+
+button.addEventListener('click', function () {
+    alert('Привіт тобі, клацальщик!');
+})
 
 
-
-// 2. За допомогою DOM отримати посилання на існуючий на сторінці елемент (div або section,
-// який заздалегідь має існувати в розмітці і містити якийсь тексту) та змінити йому 
-// колір тла на фіолетовий, а колір тексту на білий.
-
-const container = document.querySelector('.container');
-container.style.backgroundColor = 'violet';
-container.style.color = 'white';
+// 2. Створити посилання з текстом “клікни, аби з’явилась кнопка”. За натиснення на посилання
+//  поряд з ним має з’явитись новий елемент - кнопка.
 
 
+const linkEl = document.createElement('a');
+linkEl.id = 'link';
+linkEl.href = '#';
+linkEl.textContent = 'клікни, аби з’явилась кнопка';
+document.body.append(linkEl);
 
-// 3. За допомогою prompt отримати у користувача його ім’я, після чого створити і відобразити
-// на сторінці елемент <h1>, який містить текст “Вітаю, (введене користувачем ім’я)”
+linkEl.addEventListener('click', function () {
+    const funnyBtn = document.querySelector('#funny-btn');
+
+    if (funnyBtn) return;
+
+    const btnEl = document.createElement('button');
+    btnEl.id = 'funny-btn';
+    btnEl.textContent = 'Funny Button';
+    document.body.append(btnEl);
+
+})
 
 
-const userName = prompt('Enter your name', '').trim();
-function createElement(userName, defaultName = 'Гість') {
+// 3. “Лампочка”. У розмітці прописати елемент (article або div), з початковими стилями, які 
+// роблять елемент круглим, сірого кольору тла, з темно-сірою рамкою. Також прописати в 
+// розмітці кнопку, за натиснення на яку у елемента-лампочки мають змінитись стилі - тло має 
+// стати жовтим, рамка - білою.
 
-    const headerEl = document.createElement('h1');
-    
-    !userName ? headerEl.textContent = `Вітаю, ${defaultName}` : headerEl.textContent = `Вітаю, ${userName}`;
+const bulb = document.querySelector('#bulb');
+bulb.dataset.state = 'off';
+bulb.id = 'bulb';
 
-    document.body.prepend(headerEl);
 
-    return headerEl;
+function toggleBulbHandler (e) {
+    const targetBtn = e.target.closest('#toggle-btn');
+
+    if (!targetBtn) return;
+
+    if(bulb.dataset.state === 'off') {
+        bulb.style.border = '2px solid white';
+        bulb.style.backgroundColor = 'yellow';
+        bulb.dataset.state = 'on' ;
+        targetBtn.textContent = 'Turn me off!';
+
+    } else {
+        bulb.style.border = '2px solid darkgrey';
+        bulb.style.backgroundColor = 'lightgrey';
+        bulb.dataset.state = 'off' ;
+        targetBtn.textContent = 'Turn me on!';
+
+    }
+
 }
 
-createElement(userName);
-
+bulb.onclick = toggleBulbHandler;
